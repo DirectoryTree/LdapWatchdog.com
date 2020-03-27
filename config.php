422e584de197650f14924581374633017d965dc4
Steve Bauman <steven_bauman@outlook.com>
2020-03-27 13:03:48 -0400

    Added logo

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Str;
 return [
     'baseUrl' => '',
     'production' => false,
-    'siteName' => 'Docs Starter Template',
-    'siteDescription' => 'Beautiful docs powered by Jigsaw',
+    'siteName' => 'Monitor & Audit your Active Directory server.',
+    'siteDescription' => 'Watchdog monitors all changes that occur to every object in your server.',
 
     // Algolia DocSearch credentials
     'docsearchApiKey' => '',
@@ -15,7 +15,38 @@ return [
     // navigation menu
     'navigation' => require_once('navigation.php'),
 
-    // helpers
+    // Thanks to: Caleb Porzio for these methods
+    // https://github.com/livewire/docs
+    'getNextPage' => function ($page, $navigation = 'navigation') {
+        // Before: ['foo' => 'bar', 'baz' => ['children' => ['bob' => 'lob', 'law' => 'blog']]]
+        $flattenedArrayOfPagesAndTheirLables = $page->{$navigation}->map(function ($value, $key) {
+            $links = is_iterable($value) ? $value['children']->toArray() : [$key => $value];
+            return collect($links)->map(function ($path, $label) {
+                return ['path' => $path, 'label' => $label];
+            });
+        })
+            ->flatten(1);
+        // After: [['label' => 'foo', 'path' => 'bar'], ['label' => 'bob', 'path' => 'lob'], ['label' => 'law', 'path' => 'blog']]
+        $pathsByIndex = $flattenedArrayOfPagesAndTheirLables->pluck('path');
+        $currentIndex = $pathsByIndex->search(trimPath($page->getPath()));
+        $nextIndex = $currentIndex + 1;
+        return $flattenedArrayOfPagesAndTheirLables[$nextIndex] ?? null;
+    },
+    'getPreviousPage' => function ($page, $navigation = 'navigation') {
+        // Before: ['foo' => 'bar', 'baz' => ['children' => ['bob' => 'lob', 'law' => 'blog']]]
+        $flattenedArrayOfPagesAndTheirLables = $page->{$navigation}->map(function ($value, $key) {
+            $links = is_iterable($value) ? $value['children']->toArray() : [$key => $value];
+            return collect($links)->map(function ($path, $label) {
+                return ['path' => $path, 'label' => $label];
+            });
+        })
+            ->flatten(1);
+        // After: [['label' => 'foo', 'path' => 'bar'], ['label' => 'bob', 'path' => 'lob'], ['label' => 'law', 'path' => 'blog']]
+        $pathsByIndex = $flattenedArrayOfPagesAndTheirLables->pluck('path');
+        $currentIndex = $pathsByIndex->search(trimPath($page->getPath()));
+        $previousIndex = $currentIndex - 1;
+        return $flattenedArrayOfPagesAndTheirLables[$previousIndex] ?? null;
+    },
     'isActive' => function ($page, $path) {
         return Str::endsWith(trimPath($page->getPath()), trimPath($path));
     },
--- source/index.blade.php
+++ source/index.blade.php
@@ -1,23 +1,25 @@
 @extends('_layouts.master')
 
 @section('body')
-<section class="container max-w-6xl mx-auto px-6 py-10 md:py-12">
+<section class="container max-w-6xl mx-auto px-6 pb-10">
     <div class="flex flex-col-reverse mb-10 lg:flex-row lg:mb-24">
         <div class="mt-8">
-            <h1 id="intro-docs-template">{{ $page->siteName }}</h1>
+            <h1 class="text-center">{{ $page->siteName }}</h1>
 
-            <h2 id="intro-powered-by-jigsaw" class="font-light mt-4">{{ $page->siteDescription }}</h2>
+            <div class="flex justify-center">
+                <h2 class="font-light mt-4">
+                    Watchdog monitors <span class="font-extrabold">all changes</span> that occur to <span class="font-extrabold">all objects</span> in your server.
+                </h2>
+            </div>
 
             <p class="text-lg">Give your documentation a boost with Jigsaw. <br class="hidden sm:block">Generate elegant, static docs quickly and easily.</p>
 
             <div class="flex my-10">
-                <a href="/docs/getting-started" title="{{ $page->siteName }} getting started" class="bg-blue-500 hover:bg-blue-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
+                <a href="/docs/getting-started" title="{{ $page->siteName }} getting started" class="bg-gray-800 hover:bg-gray-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
 
                 <a href="https://jigsaw.tighten.co" title="Jigsaw by Tighten" class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">About Jigsaw</a>
             </div>
         </div>
-
-        <img src="/assets/img/logo-large.svg" alt="{{ $page->siteName }} large logo" class="mx-auto mb-6 lg:mb-0 ">
     </div>
 
     <hr class="block my-8 border lg:hidden">
