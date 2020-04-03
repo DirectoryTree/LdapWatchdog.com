f3bdebe3a032cd9d48e9152c4a36fa20cf586430
Steve Bauman <steven_bauman@outlook.com>
2020-04-03 11:02:02 -0400

    More updates

--- source/index.blade.php
+++ source/index.blade.php
@@ -2,22 +2,22 @@
 
 @section('body')
 <section class="container max-w-6xl mx-auto px-6 pb-10">
-    <div class="flex flex-col-reverse mb-10 lg:flex-row lg:mb-24">
-        <div class="mt-8">
-            <h1 class="text-center">{{ $page->siteName }}</h1>
+    <div class="flex justify-center flex-col-reverse mb-10 lg:flex-row lg:mb-24">
+        <div class="mt-8 text-center">
+            <h1 class="text-center">Don't be caught off guard.</h1>
 
-            <div class="flex justify-center">
-                <h2 class="font-light mt-4">
-                    Watchdog monitors <span class="font-extrabold">all changes</span> that occur to <span class="font-extrabold">all objects</span> in your server.
-                </h2>
-            </div>
+            <h2 class="font-light mt-4">
+                Let Watchdog monitor <span class="font-extrabold">all changes</span> to your Active Directory.
+            </h2>
 
-            <p class="text-lg">Give your documentation a boost with Jigsaw. <br class="hidden sm:block">Generate elegant, static docs quickly and easily.</p>
+            <div class="flex justify-center my-10">
+                <a href="/docs/getting-started" title="{{ $page->siteName }} getting started" class="bg-gray-800 hover:bg-gray-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">
+                    Get Started
+                </a>
 
-            <div class="flex my-10">
-                <a href="/docs/getting-started" title="{{ $page->siteName }} getting started" class="bg-gray-800 hover:bg-gray-600 font-normal text-white hover:text-white rounded mr-4 py-2 px-6">Get Started</a>
-
-                <a href="https://jigsaw.tighten.co" title="Jigsaw by Tighten" class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">About Jigsaw</a>
+                <a href="https://jigsaw.tighten.co" title="Jigsaw by Tighten" class="bg-gray-400 hover:bg-gray-600 text-blue-900 font-normal hover:text-white rounded py-2 px-6">
+                    Source Code
+                </a>
             </div>
         </div>
     </div>
@@ -28,17 +28,17 @@
         <div class="mb-8 mx-3 px-2 md:w-1/3">
             <img src="/assets/img/icon-window.svg" class="h-12 w-12" alt="window icon">
 
-            <h3 id="intro-laravel" class="text-2xl text-blue-900 mb-0">Templating with <br>Laravel's Blade engine</h3>
+            <h3 id="intro-laravel" class="text-2xl text-blue-900 mb-0">Track every single <br>change to all objects</h3>
 
-            <p>Blade is a powerful, simple, and beautiful templating language, and now you can use it for your static sites, not just your Laravel-powered apps.</p>
+            <p>Keep an eye on your entire directory and see when an object was changed, and it's before and after value.</p>
         </div>
 
         <div class="mb-8 mx-3 px-2 md:w-1/3">
             <img src="/assets/img/icon-terminal.svg" class="h-12 w-12" alt="terminal icon">
 
-            <h3 id="intro-markdown" class="text-2xl text-blue-900 mb-0">Use Markdown for <br>content-driven pages</h3>
+            <h3 id="intro-markdown" class="text-2xl text-blue-900 mb-0">Never be caught off<br>guard with notifications</h3>
 
-            <p>Markdown is the web’s leading format for writing articles, blog posts, documentation, and more. Jigsaw makes it painless to work with Markdown content.</p>
+            <p>Watchdog notifies you of everything important, such as password updates, account lockouts, membership changes and more.</p>
         </div>
 
         <div class="mx-3 px-2 md:w-1/3">
